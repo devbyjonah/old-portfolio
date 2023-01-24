dcf144aa765189b809fe2f5c43fccbeb05e9ea44
jonah wagner <jonahwagner231@gmail.com>
2023-01-23 19:39:51 -0800

    set inter as global font

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,12 @@
 import Header from "../components/Header";
 
+import { Inter } from "@next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Layout({ children }) {
   return (
     <>
-      <main>{children}</main>
+      <main className={inter.className}>{children}</main>
     </>
   );
 }
