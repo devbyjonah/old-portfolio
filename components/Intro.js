82b3096adc5cbb71e6a179d357cab13209840f4a
jonah wagner <jonahwagner231@gmail.com>
2023-01-24 15:55:01 -0800

    cleaning up styling

--- components/Intro.js
+++ components/Intro.js
@@ -10,10 +10,7 @@ export default function Intro() {
 				<div className="row h-100 w-100">
 					<div className="col-2 d-none d-lg-block"></div>
 					<div className="col position-relative">
-						<div
-							className="position-absolute w-100 d-flex"
-							style={{ top: 100 + "px" }}
-						>
+						<div className="w-100 d-flex justify-content-center mt-5">
 							<div>
 								<Image
 									width={244}
@@ -23,20 +20,26 @@ export default function Intro() {
 									className="float-start rounded-circle p-1"
 								/>
 							</div>
-							<div className="mt-auto mb-auto ms-5">
+							<div className="mt-auto mb-auto ms-md-5">
 								<span className="fw-lighter p-1">
 									Hello! My name is
-									<h2 className="fs-1 fw-lighter">
+									<h2 className="fs-1 fw-light">
 										Jonah Wagner.
 									</h2>
 								</span>
 								<span className="fw-lighter p-1">
 									I am a creative Software Engineer with
 								</span>
-								<h2 className="fs-1 fw-lighter p-1">
+								<h2 className="fs-1 fw-light p-1">
 									a passion for learning...
 								</h2>
 							</div>
+						</div>
+						<div className="m-3 mt-5">
+							<h1 className="fw-bolder">
+								I am a full stack web developer using the MERN
+								stack to build web apps.
+							</h1>
 						</div>
 					</div>
 					<div className="col-2 d-none d-lg-block"></div>
