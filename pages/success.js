2dbcfd74144a45dbccac711b71cf5f65deffdce8
jonah wagner <jonahwagner231@gmail.com>
2023-01-28 19:43:28 -0800

    improved form with custom success page

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,20 @@
-export default function success() {
-	return <div>success</div>;
+import { useContext } from "react";
+import { DarkModeContext } from "@/context/DarkModeContext";
+
+export default function Success() {
+	const { darkMode } = useContext(DarkModeContext);
+
+	return (
+		<main
+			className={
+				(darkMode ? "text-bg-dark" : "text-bg-light") +
+				" min-vh-100 d-flex align-items-center justify-content-center"
+			}
+		>
+			<div>
+				<h1 className="text-center">Thanks for your message!</h1>
+				<p className="text-center">I&apos;ll get back to you ASAP</p>
+			</div>
+		</main>
+	);
 }
